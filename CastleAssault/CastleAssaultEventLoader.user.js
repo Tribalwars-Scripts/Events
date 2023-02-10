ff995996f2a90eb16f0bb3dad26b76f53fa0a672
Bruno Preto <brunommpreto@gmail.com>
2023-02-10 04:15:59 +0000

    Version 1.0.2

--- CastleAssault/CastleAssaultEventLoader.user.js
+++ CastleAssault/CastleAssaultEventLoader.user.js
@@ -6,8 +6,8 @@
 // @email               brunommpreto@disroot.org
 // @description 	    Script to automatically go thru the Castle Assault Event
 // @author		        Bruno Preto (bonobobo#1694)
-// @include              https://**.tribalwars.**/game.php?**&screen=event_assault
-// @version     	    1.0.1
+// @include             https://**.tribalwars.**/game.php?**&screen=event_assault
+// @version     	    1.0.2
 // @copyright           2023, brunommpreto (https://openuserjs.org/)
 // @license             AGPL-3.0-or-later
 // @supportURL          https://github.com/Tribalwars-Scripts/
@@ -16,8 +16,13 @@
 // @grant               GM_addStyle
 // @grant               GM_getValue
 // @grant               unsafeWindow
+// @run-at              document-end
 // @require             http://code.jquery.com/jquery-1.12.4.min.js
 // ==/UserScript==
+
+const Cangelog = {
+    "1.0.2" : "Added a few things to avoid performing a lot of requests to TW server"
+}
 
 /***************************** Configuration ***************************
 
@@ -31,10 +36,14 @@ const AssaultOptions = ['naval','assault', 'catapult']
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms || DEF_DELAY));
 }
+function getMilliseconds(hrs, min, sec) {
+    return ((hrs * 60 * 60 + min * 60 + sec) * 1000);
+}
 
 (function () {
     'use strict';
     console.log("-- Castle Event Script Started --");
+    console.log(Cangelog["1.0.2"])
     DoAssault().then(r => {
         let msg = "Successfully Performed  " + r  + " Assaults\n" + "Next Assault in 60 minutes."
         UI.SuccessMessage(msg, 1000);
@@ -45,7 +54,7 @@ function sleep(ms) {
             let msg = "Successfully Performed  " + r  + " Assaults\n" + "Next Assault in 60 minutes."
             UI.SuccessMessage(msg, 1000);
         });
-    }, 6e10);
+    }, getMilliseconds(1,0,0));
 })();
 
 async function DoAssault(){

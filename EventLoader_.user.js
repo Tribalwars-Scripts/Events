fcc774728eea5002cb924792151b6ba696973aff
Bruno Preto <brunommpreto@gmail.com>
2023-04-12 23:12:01 +0100

    BarricadeEvent

--- EventLoader_.user.js
+++ EventLoader_.user.js
@@ -51,7 +51,7 @@ $.ajax({
 	console.info('Fetching the Event Script from the main repository.');
 	$.ajax({
 		type: 'GET',
-		url: 'https://rawcdn.githack.com/Tribalwars-Scripts/Events/main/' + ScriptName.trim() + '/' + ScriptName.trim() + 'Loader.js',
+		url: 'https://rawcdn.githack.com/Tribalwars-Scripts/Events/main/' + ScriptName.replace(' ','') + '/' + ScriptName.replace(' ','') + 'Loader.js',
 		dataType: 'script',
 		cache: false,
 	});

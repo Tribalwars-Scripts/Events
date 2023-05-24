dc50295e0e911b6f26e23ae5bfc85faf8c2122f6
Bruno Preto <brunommpreto@gmail.com>
2023-05-24 18:50:56 +0100

    Fix name

--- EventLoader.user.js
+++ EventLoader.user.js
@@ -7,7 +7,7 @@
 // @description 	    Script to automatically go thru any TribalWars Event
 // @author		        Bruno Preto (bonobobo#1694)
 // @include             https://**.tribalwars.**/game.php?**&screen=event_**
-// @version     	    1.1.1
+// @version     	    1.1.2
 // @copyright           2023, brunommpreto (https://openuserjs.org/)
 // @license             AGPL-3.0-or-later
 // @supportURL          https://github.com/Tribalwars-Scripts/
